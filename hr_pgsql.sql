c601b8dd27d594e88a65ca4a0c311ecbc8ef37ba
Javier Alejandro Arteaga Gonzales <132113806+javierarteagagonzales@users.noreply.github.com>
2024-06-01 16:01:37 -0500

    db hr

---
+++ hr_pgsql.sql
@@ -0,0 +1,17 @@
+/*PARTE I: ACADÉMICO
+1. Elabore una sentencia SELECT que permita capturar los datos principales
+(nombre, apellido y teléfono) de todos los alumnos que viven en la ciudad de “LOS
+ANGELES” y cuyo apellido contiene la letra “r”.*/
+/*2. Elabore una sentencia SELECT que permita imprimir un reporte con los docentes
+(considere solamente nombre y apellido) y la cantidad de secciones que tiene asignado
+cada uno de ellos. Muestre solamente información de los docentes que tengan más de
+una sección.*/
+/*3. Elabore una sentencia SELECT que permita obtener una “boleta de matrícula”
+para un alumno determinado. Debe mostrarse los cursos y secciones en los cuales se
+encuentra matriculado, así como el profesor a cargo. Considere los campos que crea
+conveniente.*/
+/*4. Muestre un reporte de disponibilidad de todas las secciones. Debe considerar el
+nombre del curso, el nombre del docente a cargo, la capacidad de la sección y la cantidad
+de vacantes disponibles.*/
+/*5. Muestre los datos de todos aquellos cursos que tengan una cantidad total de
+alumnos matriculados menor que el promedio general considerando todos los cursos.*/
