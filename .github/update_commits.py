d7e14e5705734a85561552bad99ef8aecbcd542e
Javier Alejandro Arteaga Gonzales <132113806+javierarteagagonzales@users.noreply.github.com>
2024-06-29 19:00:30 -0500

    Create update_commits.py

---
+++ .github/update_commits.py
@@ -0,0 +1,21 @@
+import requests
+
+def update_readme():
+    repo_owner = 'owner_username'
+    repo_name = 'repo_name'
+    api_url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/commits'
+
+    # Obtener los commits por autor
+    response = requests.get(api_url)
+    commits = response.json()
+
+    # Procesar los commits y actualizar README.md
+    with open('README.md', 'w') as readme:
+        readme.write('# Commits por Autor\n\n')
+        for commit in commits:
+            author = commit['commit']['author']['name']
+            message = commit['commit']['message']
+            readme.write(f'- **{author}**: {message}\n')
+
+if __name__ == '__main__':
+    update_readme()
